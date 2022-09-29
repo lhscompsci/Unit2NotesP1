32bcd4b2d56b03d1eb6a5d229e93435e8011358c
Sam Black <sam.black@lubbockisd.org>
2022-09-29 08:46:30 -0500

    Added notes on using Math class and generating random numbers

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,22 @@ public class Main {
         int ans = dudekid.multiplier(4,21);
         out.println("the answer is "+ ans);
 
+
+        //anytime you need to use a method from the Math class
+        //just start  Math.    and let the list pop up
+     //   Math.
+
+        //Random Numbers  (RNG)
+        //pseudorandom numbers (PNG)
+
+        //Math.random()  <== 0.0 up to but not including 1.0
+
+        //    (int)(Math.random() * [range]  + lower)
+        //                  upper - lower + 1
+
+        //Roll a d20
+        int value = (int)(Math.random()*20 + 1);
+        out.println("You rolled: "+ value);
+
     }
 }
