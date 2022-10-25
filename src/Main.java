a90b7b5d84c7d7c524b06bb6a73b2ee40589f84d
Sam Black <sam.black@lubbockisd.org>
2022-10-25 08:59:49 -0500

    added String notes

--- src/Main.java
+++ src/Main.java
@@ -39,5 +39,32 @@ public class Main {
         int value = (int)(Math.random()*20 + 1);
         out.println("You rolled: "+ value);
 
+        //Strings -- collections of characters enclosed by quotation marks "lakdshflakdsjf"
+        //              0123456789  -- position
+        String myStr = "Howdy yall";
+        // Strings are immutable
+
+        out.println(myStr.length());    //total quantity
+        out.println(myStr.indexOf("y"));    //where is it in the string -- first occurrence
+        out.println(myStr.indexOf("How"));  //where it begins
+        out.println(myStr.lastIndexOf("y"));    //looks from the right
+        out.println(myStr.charAt(7));       //what char is at the given location
+        out.println(myStr.substring(7));    //returns a portion of the original string beginning at the given loc
+                                                        // all the way to the end
+        out.println(myStr.substring(3,8));      //returns a portion starting at 3 and goes up to but not including 8
+        myStr.toUpperCase();        //returns the original ALL UPPERCASED
+        out.println(myStr);
+        myStr = myStr.toUpperCase();
+        out.println(myStr);
+        //compareTo  vs  equals
+        String s1 = "one";
+        String s2 = "two";
+        out.println(s1.equals("one"));      //boolean -- checks to see if same string
+        out.println(s1.compareTo(s2));      //
+        out.println(s2.compareTo(s1));      //
+
+
+
+
     }
 }
